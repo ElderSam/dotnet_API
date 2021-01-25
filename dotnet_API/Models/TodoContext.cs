18047933b5a739c9772adc4f05fd635576b04fee
ElderSam <eldersamuel98@gmail.com>
2021-01-24 23:28:50 -0300

    Register the database context

--- dotnet_API/Models/TodoContext.cs
+++ dotnet_API/Models/TodoContext.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
 
-namespace dotnet_API.Models
+namespace TodoApi.Models
 {
     public class TodoContext : DbContext
     {
--- dotnet_API/Startup.cs
+++ dotnet_API/Startup.cs
@@ -1,18 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+//using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
 
-namespace dotnet_API
+namespace TodoApi
 {
     public class Startup
     {
@@ -26,12 +21,14 @@ namespace dotnet_API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt =>
+                                              opt.UseInMemoryDatabase("TodoList"));
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
+            /*services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "dotnet_API", Version = "v1" });
-            });
+            });*/
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +37,8 @@ namespace dotnet_API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "dotnet_API v1"));
+                /*app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "dotnet_API v1"));*/
             }
 
             app.UseHttpsRedirection();
